f905800de7a2d9006d60ddf329416712d083c3d1
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-01 14:08:51 +0000

    Finished largest number selector

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Selects largest number from a set of 3
+ * @author Jaden Ramcharan
  */
 public class Main {
 
@@ -9,7 +10,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Gets number from user
+    System.out.println("Please enter three integers on separate lines");
+    int num1 = input.nextInt();
+    int num2 = input.nextInt();
+    int num3 = input.nextInt();
+
+    //selects largest number
+    if(num1 >= num2 + num3){
+      System.out.println("Maximum: " + num1);
+    }
+    if(num2 >= num1 + num3){
+      System.out.println("Maximum: " + num2);
+    }
+    if(num3 >= num2 + num1){
+      System.out.println("Maximum: " + num3);
+    }else{
+      System.out.println("Maximum : " + num1);
+    }
+    
     
   }
 }
